7ebddeb54948b0c7f184b9b2b46b1bab8a1b3367
Musfik Tamim <musfiktamim@gmail.com>
2024-09-19 20:53:15 +0600

    hello world

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+function page() {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default page
+
+
+
+// relative flex-col mx-auto  text-black font-light text-sm  h-56 w-56 rounded-md bg-white dark:bg-zinc-800 flex items-center justify-center group overflow-hidden  
